b2406b43107546492930b498941324df014b8a43
xtr4nge <xtr4nge@gmail.com>
2014-03-11 19:07:58 +0000

    minor fix

--- _info_.php
+++ _info_.php
@@ -1,6 +1,6 @@
 <?
 $mod_name="responder";
-$mod_version="1.1";
+$mod_version="1.2";
 $mod_logs="/usr/share/FruityWifi/logs/responder.log"; 
 $mod_logs_history="/usr/share/FruityWifi/www/modules/responder/includes/logs/";
 $mod_path="/usr/share/FruityWifi/www/modules/responder";
@@ -11,7 +11,7 @@ $mod_alias="Responder";
 $bin_danger = "/usr/share/FruityWifi/bin/danger";
 $bin_sudo = "/usr/bin/sudo";
 $bin_sh = "/bin/sh";
-$bin_echo = "/bin_echo";
+$bin_echo = "/bin/echo";
 $bin_grep = "/usr/bin/ngrep";
 $bin_killall = "/usr/bin/killall";
 $bin_cp = "/bin/cp";
