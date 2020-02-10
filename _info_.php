0e74e183e5b5147c7ad6d441ee64b3f3278a6e2c
xtr4nge <xtr4nge@gmail.com>
2020-02-10 10:54:12 +0000

    minor update (version)

--- _info_.php
+++ _info_.php
@@ -1,6 +1,6 @@
 <?
 $mod_name="responder";
-$mod_version="2.b";
+$mod_version="1.6";
 $mod_path="/usr/share/fruitywifi/www/modules/$mod_name";
 $mod_logs="$log_path/$mod_name.log"; 
 $mod_logs_history="$mod_path/includes/logs/";
